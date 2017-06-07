fc412c2794a1de35efc180ed0667984af858c12a
Brandon R Barnett <BBarnett@rightpoint.com>
2017-06-07 13:31:10 -0500

    Cleanup

--- DotNetCoreSpa.Api/Startup.cs
+++ DotNetCoreSpa.Api/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DotNetCoreSpa.Api.Extensions.AzureAd;
+﻿using DotNetCoreSpa.Api.Extensions.AzureAd;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace DotNetCoreSpa.Api
 {
