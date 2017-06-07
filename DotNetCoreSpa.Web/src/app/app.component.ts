210a5d39de0e7c0f9d2d44713a03d91fd347a6f8
Brandon R Barnett <BBarnett@rightpoint.com>
2017-06-07 15:36:37 -0500

    Bootstrap, collapsing app.component

--- DotNetCoreSpa.Web/src/app/app.component.ts
+++ DotNetCoreSpa.Web/src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    template: `
+        <header class="container">
+            <nav>
+                <a routerLink="/" routerLinkActive="active">Home</a>
+                <a routerLink="/values" routerLinkActive="active">Values</a>
+            </nav>
+        </header>
+
+        <main id="body" class="container">
+            <router-outlet></router-outlet>
+        </main>
+
+        <footer>
+    
+        </footer>
+    `,
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 }
